2dc828c71750283501ba210767980e230077ed6e
Felype-Henrique <felypehenrique25@gmail.com>
2022-03-23 10:47:41 -0300

    DashboadAdd

--- src/pages/Dashboard/components/Card/index.tsx
+++ src/pages/Dashboard/components/Card/index.tsx
@@ -1,17 +1,27 @@
 import React from "react";
+import tw from "tailwind-styled-components"
 import { Button } from "../../../../components/atoms/Button";
 
 export const Card = ({ title, category, cost}) => {
   return (
-    <article className="bg-white p-4 shadow-md h-40 relative flex justify-between flex-col gap-2">
-      <Button variant="ghost" className="absolute top-0 right-0">
-        Excluir
-      </Button>
+    <CardContainer>
       <h2 className="font-bold text-xl">{title}</h2>
       <p className="px-4 h-8 flex items-center justify-center text-purple-900 bg-purple-100 self-start rounded-full">
         {category}
       </p>
       <p className="font-medium">{cost}</p>
-    </article>
+      <footer className="w-full flex justify-between">
+      <Button variant="ghost" className="">
+        Editar
+      </Button>
+      <Button variant="ghost" className="">
+        Excluir
+      </Button>
+      </footer>
+    </CardContainer>
   );
 };
+
+export const CardContainer = tw.article`
+bg-white p-4 shadow-md relative flex justify-between flex-col gap-4
+`
---
+++ src/pages/DashboardAdd/index.tsx
@@ -0,0 +1,25 @@
+import { Button } from "../../components/atoms/Button"
+import { InputText } from "../../components/molecules/inputText"
+import { Link } from "../../components/molecules/Link"
+import { SidebarHero } from "../../components/organism/SidebarHero"
+import { TemplateContainer, TemplateContent, TemplateMainHeroSection} from "../../components/templates"
+
+export const DashboardAdd = () => {
+  return (
+    <TemplateContainer>
+        <TemplateContent>
+            <SidebarHero/>
+            <TemplateMainHeroSection>
+              <h1 className="font-bold text-3xl">Cadastre nova despesa</h1>
+              <form className="flex flex-col w-full gap-4">
+                <InputText label="Despesa: " type='text'/>
+                <InputText label="Categoria: " type='text'/>
+                <InputText label="Valor: " type='number'/>
+                <Button>Cadastrar</Button>
+              </form>
+              <Link href="/dashboard">Voltar</Link>
+            </TemplateMainHeroSection>
+        </TemplateContent>
+    </TemplateContainer>
+  )
+}
